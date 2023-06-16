6b3bdc057481b6dfc941dca61dc8ae8ca097fc04
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-16 09:24:31 +0200

    add repo

---
+++ src/main/java/com/project/JAVAU2W3PROJECT/JavaU2W3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.JAVAU2W3PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaU2W3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaU2W3ProjectApplication.class, args);
+	}
+
+}
